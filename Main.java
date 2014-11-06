d356193bcb4e46626532c214c4319a9c6dc99b0b
shramko82 <shramko82@gmail.com>
2014-11-06 12:39:24 +0200

    Issue 4
    Reduced casting toLowerCase()
    separate one complicated IF to 2 IFs

--- Main.java
+++ Main.java
@@ -81,11 +81,12 @@ public class Main {
 
         int count = 0;
         for (int i = 0; i < str.length(); i++) {
-            if (((Character.toLowerCase(str.charAt(i)) == 'y') || (Character
-                    .toLowerCase(str.charAt(i)) == 'z'))
-                    && ((i == str.length()) || (!Character.isLetter(str
-                            .charAt(i + 1)))))
-                count++;
+            char c = Character.toLowerCase(str.charAt(i));
+            if ((c == 'y') || (c == 'z'))
+                if ((i == str.length())
+                        || (!Character.isLetter(str.charAt(i + 1))))
+                    count++;
+
         }
 
         return count;
