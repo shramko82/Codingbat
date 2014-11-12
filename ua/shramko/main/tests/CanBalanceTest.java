e825b2606bac1e0eefd9524ae8db0ef6d42f800a
shramko82 <shramko82@gmail.com>
2014-11-12 13:52:35 +0200

    removed cardinal letters in packages
    removed cardinal letters in packages

---
+++ ua/shramko/main/tests/CanBalanceTest.java
@@ -0,0 +1,19 @@
+package ua.shramko.main.tests;
+
+import ua.shramko.main.CanBalance;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CanBalanceTest {
+
+	@Test
+	public void testCanBalance() {
+		CanBalance cb = new CanBalance();
+		assertTrue(cb.canBalance(new int[] { 1, 1, 1, 2, 1 }));
+		assertFalse(cb.canBalance(new int[] { 13, 6, 1, 2, 3 }));
+		assertTrue(cb.canBalance(new int[] { 15,25,10,10,10,10 }));
+	}
+
+}
