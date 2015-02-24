d62ca75cbb368fb27a594aa3cc09f027dce65cd0
shramko82 <shramko82@gmail.com>
2015-02-24 10:43:52 +0200

    WithoutX2 added
    WithoutX2 added

---
+++ ua/shramko/main2/WithoutX2.java
@@ -0,0 +1,31 @@
+package ua.shramko.main2;
+
+public class WithoutX2 {
+
+//	Given a string, if one or both of the first 2 chars is 'x', 
+//	return the string without those 'x' chars, and otherwise return the string unchanged. 
+//	This is a little harder than it looks. 
+//
+//			withoutX2("xHi") → "Hi"
+//			withoutX2("Hxi") → "Hi"
+//			withoutX2("Hi") → "Hi"
+	
+	public static String withoutX2(String str) {
+		if (str == null) {
+			System.err.println("Inrut string should not be null");
+			return null;
+		}
+		
+		if ((str.length() > 1) && (str.charAt(1) == 'x'))  {
+			str = str.substring(0, 1)+str.substring(2, str.length());
+		}
+		if ((str.length() > 0) && (str.charAt(0) == 'x'))  {
+			str = str.substring(1, str.length());
+		}
+		return str; 
+	}
+	public static void main(String[] args) {
+		withoutX2("Hxi");
+	}
+	
+}
