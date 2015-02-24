c13f7fc575d8329fbdb0f8e5d91f950ad7865369
shramko82 <shramko82@gmail.com>
2015-02-24 11:11:10 +0200

    SumLimit
    SumLimit added

---
+++ ua/shramko/main2/SumLimit.java
@@ -0,0 +1,24 @@
+package ua.shramko.main2;
+
+/*Given 2 non-negative ints, a and b, return their sum, so long as the sum has 
+the same number of digits as a. If the sum has more digits than a, just return a without b. 
+(Note: one way to compute the number of digits of a non-negative int n is to convert it 
+to a string with String.valueOf(n) and then check the length of the string.) 
+
+sumLimit(2, 3) → 5
+sumLimit(8, 3) → 8
+sumLimit(8, 1) → 9 */
+
+public class SumLimit {
+
+	public int sumLimit(int a, int b) {
+		  if ((a < 0) || (b < 0)) {
+			  System.err.println("Input number should be not negative");
+			  return 0;
+		  }
+		  
+		  int c = a+b;
+		  return (String.valueOf(c).length() > String.valueOf(a).length()) ? a : c;
+		  
+	}
+}
