0892e0e2a836d61fd9a0245d58824afb330f1702
shramko82 <shramko82@gmail.com>
2015-03-04 23:31:44 +0200

    Description at Array667
    Added Description at Array667

--- ua/shramko/main2/Array667.java
+++ ua/shramko/main2/Array667.java
@@ -1,6 +1,11 @@
 package ua.shramko.main2;
 
 public class Array667 {
+//	Given an array of ints, return the number of times that two 6's are next to each other in the array. Also count instances where the second "6" is actually a 7. 
+//
+//			array667({6, 6, 2}) → 1
+//			array667({6, 6, 2, 6}) → 1
+//			array667({6, 7, 2, 6}) → 1
 	public int array667(int[] nums) {
 		if (nums.length < 2) {
 			return 0;
