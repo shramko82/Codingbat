65946b4941f6057ae4aa7d66076a68b81360d0a3
shramko82 <shramko82@gmail.com>
2015-03-05 17:09:10 +0200

    LuckySum
    LuckySum added

---
+++ ua/shramko/main2/LuckySum.java
@@ -0,0 +1,31 @@
+package ua.shramko.main2;
+
+		/*Given 3 int values, a b c, return their sum. 
+		However, if one of the values is 13 then it does not count 
+		towards the sum and values to its right do not count. 
+		So for example, if b is 13, then both b and c do not count. 
+
+luckySum(1, 2, 3) → 6
+luckySum(1, 2, 13) → 3
+luckySum(1, 13, 3) → 1
+*/
+public class LuckySum {
+	
+	public int luckySum(int a, int b, int c) {
+		  int result = 0;
+		  if (a==13) 
+			  return result;
+		  else
+			  result += a;
+		  if (b==13) 
+			  return result;
+		  else
+			  result += b;
+		  if (c==13) 
+			  return result;
+		  else
+			  result += c;
+		  return result;
+	}
+
+}
